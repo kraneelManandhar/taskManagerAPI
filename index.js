91dacede34f69a1a879c9be70501db2e29e8d800
kraneelManandhar <mydarling3547@gmail.com>
2025-06-29 12:25:50 +0545

    Removed post,get and made it very simple Just covering basics of node js

--- index.js
+++ index.js
@@ -4,49 +4,35 @@ const fs = require('fs');
 const port = 2000;
 const storedData = 'tasks.json';
 
+let initialTasks = [
+  {
+    "id": 1,
+    "title": "Do your homework",
+    "completed": false
+  }
+];
+
 const server = http.createServer((req, res) => {
-  if (req.method === 'GET' && req.url === '/tasks') {
+  if (req.url === '/tasks') {
+
     fs.readFile(storedData, 'utf8', (err, data) => {
       if (err) {
-        res.writeHead(500);
         res.end('Error reading.');
         return;
       }
-      res.writeHead(200, { 'Content-Type': 'application/json' });
       res.end(data);
     });
-  }
-  else if (req.method === 'POST' && req.url === '/tasks') {
-    let body = '';
-    req.on('data', chunk => {
-      body += chunk.toString();
+
+  }else if ( req.url === '/tasks/add') {
+    fs.writeFile(storedData, JSON.stringify(initialTasks, null, 2), err => {
+      if (err) {
+        res.end('Error saving task');
+        return;
+      }
+      res.end("Data is stored.");
     });
-    req.on('end', () => {
-      const task = JSON.parse(body);
 
-      fs.readFile(storedData, 'utf8', (err, data) => {
-        let tasks = [];
-        if (!err && data) {
-          tasks = JSON.parse(data);
-        }
-
-        task.id = tasks.length + 1;
-        tasks.push(task);
-
-        fs.writeFile(storedData, JSON.stringify(tasks, null, 2), err => {
-          if (err) {
-            res.writeHead(500);
-            res.end('Error saving task');
-            return;
-          }
-          res.writeHead(201, { 'Content-Type': 'application/json' });
-          res.end(JSON.stringify(task));
-        });
-      });
-    });
-  }
-  else {
-    res.writeHead(404);
+  }else{
     res.end('Route not found');
   }
 });
